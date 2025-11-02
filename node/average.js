aa17538ac1ab3de1109d90510df1c27cf5433ed5
Daniel MK <danielmk98@hotmail.com>
2025-11-02 17:16:05 -0300

    node, npm and vite

---
+++ node/average.js
@@ -0,0 +1,7 @@
+const nota1 = parseInt(process.argv[2]);
+const nota2 = parseInt(process.argv[3]);
+
+const media = (nota1 + nota2) / 2;
+
+console.log(media);
+
---
+++ npm/prime.js
@@ -0,0 +1,10 @@
+const num = parseInt(process.argv[2]);
+
+// TODO
+const isNumPrime = true;
+
+if (isNumPrime) {
+    console.log(num, "é primo");
+} else {
+    console.log(num, "não é primo");
+}
